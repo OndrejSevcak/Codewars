080ceea21c1c9df7bd740195c42a2f0b748a2390
OndrejS <ondrejsevcak@gmail.com>
2024-11-12 18:34:52 +0100

    added CountingDuplicates

---
+++ Codewars/6kyu/CountingDuplicates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars._6kyu
+{
+    //Write a function that will return the count of distinct case-insensitive alphabetic characters
+    //and numeric digits that occur more than once in the input string.
+    //The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+    //"abcde" -> 0
+    //"aabBcde" -> 2
+    //"Indivisibilities" -> 2
+
+    public class CountingDuplicates
+    {
+        public static int Run(string input)
+        {
+            return input.ToLower().ToCharArray().GroupBy(c => c).Where(g => g.Count() > 1).Count();
+        }
+    }
+}
---
+++ CodewarsTests/6kyu/CountingDuplicatesTest.cs
@@ -0,0 +1,46 @@
+﻿using Codewars._6kyu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodewarsTests._6kyu
+{
+    public class CountingDuplicatesTest
+    {
+        [Fact]
+        public void KataTests()
+        {
+            Assert.Equal(0, CountingDuplicates.Run(""));
+            Assert.Equal(0, CountingDuplicates.Run("abcde"));
+            Assert.Equal(2, CountingDuplicates.Run("aabbcde"));
+            Assert.Equal(2, CountingDuplicates.Run("aabBcde"));
+            Assert.Equal(1, CountingDuplicates.Run("Indivisibility"));
+            Assert.Equal(2, CountingDuplicates.Run("Indivisibilities"));
+        }
+
+        [Fact]
+        public void RandomTests()
+        {
+            var random = new Random();
+            string randomStr;
+
+            for (int i = 0; i < 10; i++)
+            {
+                randomStr =
+                  String.Join("", Enumerable.Range(0, 20).Select((o, x) => (char)random.Next('a', 'z') + (char)random.Next('A', 'Z')));
+
+                var expected = Solution(randomStr);
+                Assert.Equal(expected, CountingDuplicates.Run(randomStr));
+            }
+        }
+
+        public static int Solution(string str)
+        {
+            str = String.Join("", str.ToLower().OrderBy(c => c));
+            return Regex.Matches(str, @"(.)\1+").Count;
+        }
+    }
+}
